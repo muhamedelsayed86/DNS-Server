fb6bb4080d3b535a971711508914b30b507a63ee
Muhammed Elsayed <mmohammedahmmed860@gmail.com>
2026-07-17 17:55:20 +0300

    forward queries to external server and merge answers

--- src/dns_server.py
+++ src/dns_server.py
@@ -1,36 +1,43 @@
 import socket
+import sys
 
 def parse_domain_name(buf, offset):
     labels = []
     while True:
         length = buf[offset]
-        
         if length == 0:
             labels.append(b'\x00')
             offset += 1
             break
-            
         elif (length & 0xC0) == 0xC0:
-            pointer_bytes = buf[offset:offset+2]
-            pointer_offset = int.from_bytes(pointer_bytes, 'big') & 0x3FFF
-            
+            pointer_offset = int.from_bytes(buf[offset:offset+2], 'big') & 0x3FFF
             pointed_name, _ = parse_domain_name(buf, pointer_offset)
             labels.append(pointed_name)
             offset += 2
             break
-            
         else:
             offset += 1
             labels.append(bytes([length]) + buf[offset:offset+length])
             offset += length
-            
     return b''.join(labels), offset
 
 def main():
-    print("Moe tells you DNS server is running...")
+    resolver_ip = "8.8.8.8" 
+    resolver_port = 53
+    if "--resolver" in sys.argv:
+        idx = sys.argv.index("--resolver")
+        resolver_address = sys.argv[idx + 1]
+        resolver_ip, port_str = resolver_address.split(':')
+        resolver_port = int(port_str)
+        
+    print(f"Moe tells you server is running on 127.0.0.1:2053")
+    print(f"Forwarding queries to {resolver_ip}:{resolver_port}...")
 
     udp_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     udp_socket.bind(("127.0.0.1", 2053))
+    
+    forward_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    forward_socket.settimeout(2)
     
     while True:
         try:
@@ -46,40 +53,71 @@ def main():
 
             qdcount_int = int.from_bytes(buf[4:6], 'big')
             
-            qdcount = qdcount_int.to_bytes(2, byteorder='big') 
-            ancount = qdcount_int.to_bytes(2, byteorder='big') 
-            nscount = (0).to_bytes(2, byteorder='big') 
-            arcount = (0).to_bytes(2, byteorder='big')  
-
-            header = packet_id + flags + qdcount + ancount + nscount + arcount
-
             offset = 12
-            questions = b""
-            answers = b""
-            
+            questions = []
             for _ in range(qdcount_int):
                 uncompressed_name, offset = parse_domain_name(buf, offset)
-                
                 qtype = buf[offset:offset+2]
                 qclass = buf[offset+2:offset+4]
                 offset += 4
+                questions.append((uncompressed_name, qtype, qclass))
+            
+            merged_answers = b""
+            ans_count = 0
+            
+            for q in questions:
+                uncompressed_name, qtype, qclass = q
                 
-                questions += uncompressed_name + qtype + qclass
+                f_flags_int = (0 << 15) | (opcode << 11) | (rd << 8) | 0 
+                f_flags = f_flags_int.to_bytes(2, 'big')
+                f_qdcount = (1).to_bytes(2, 'big')
+                f_ancount = (0).to_bytes(2, 'big')
+                f_nscount = (0).to_bytes(2, 'big')
+                f_arcount = (0).to_bytes(2, 'big')
                 
-                atype = (1).to_bytes(2, byteorder='big')
-                aclass = (1).to_bytes(2, byteorder='big')
-                ttl = (60).to_bytes(4, byteorder='big')          
-                rdlength = (4).to_bytes(2, byteorder='big')      
-                rdata = b"\x08\x08\x08\x08" # IP : 8.8.8.8
+                f_header = packet_id + f_flags + f_qdcount + f_ancount + f_nscount + f_arcount
+                f_question = uncompressed_name + qtype + qclass
                 
-                answers += uncompressed_name + atype + aclass + ttl + rdlength + rdata
+                forward_query = f_header + f_question
+                
+                forward_socket.sendto(forward_query, (resolver_ip, resolver_port))
+                res_buf, _ = forward_socket.recvfrom(1024)
+                
+                res_ancount = int.from_bytes(res_buf[6:8], 'big')
+                
+                res_offset = 12
+                _, res_offset = parse_domain_name(res_buf, res_offset)
+                res_offset += 4 
+                
+                for _ in range(res_ancount):
+                    ans_name, res_offset = parse_domain_name(res_buf, res_offset)
+                    ans_type = res_buf[res_offset:res_offset+2]
+                    ans_class = res_buf[res_offset+2:res_offset+4]
+                    ans_ttl = res_buf[res_offset+4:res_offset+8]
+                    
+                    ans_rdlength_int = int.from_bytes(res_buf[res_offset+8:res_offset+10], 'big')
+                    ans_rdlength = res_buf[res_offset+8:res_offset+10]
+                    res_offset += 10
+                    
+                    ans_rdata = res_buf[res_offset:res_offset+ans_rdlength_int]
+                    res_offset += ans_rdlength_int
+                    
+                    merged_answers += ans_name + ans_type + ans_class + ans_ttl + ans_rdlength + ans_rdata
+                    ans_count += 1
 
-            response = header + questions + answers
-            udp_socket.sendto(response, source)
+            resp_qdcount = qdcount_int.to_bytes(2, 'big')
+            resp_ancount = ans_count.to_bytes(2, 'big')
+            resp_nscount = (0).to_bytes(2, 'big')
+            resp_arcount = (0).to_bytes(2, 'big')
+
+            resp_header = packet_id + flags + resp_qdcount + resp_ancount + resp_nscount + resp_arcount
+            resp_questions = b"".join([q[0] + q[1] + q[2] for q in questions])
+            
+            final_response = resp_header + resp_questions + merged_answers
+            udp_socket.sendto(final_response, source)
             
         except Exception as e:
             print(f"Error: {e}")
-            break
 
 if __name__ == "__main__":
     main()
